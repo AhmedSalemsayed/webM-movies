febbc19099a2690aa44b2481d6936620aea91db4
ahmedsalemsayed <ahmedtaco1996@gmail.com>
2025-10-13 13:48:57 +0300

    refactored header element

--- js/main.js
+++ js/main.js
@@ -33,12 +33,14 @@ let navbar = document.querySelector(".navbar");
 
 
 menuIcon.onclick = () => {
-  navbar.classList.toggle("active");
+  navbar.classList.toggle("flex");
+  navbar.classList.toggle("hidden");
 }
 window.onscroll = () => {
   navbar.classList.remove("active");
 }
 window.addEventListener('scroll', () => {
-    header.classList.toggle("shadow" , window.scrollY > 0);
+    header.classList.toggle("bg-white", window.scrollY > 0);
+    header.classList.toggle("shadow-md", window.scrollY > 0);
 });
 
